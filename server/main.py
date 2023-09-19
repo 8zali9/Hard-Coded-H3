9af347d72aa6ef523e4c39bc7d9d515119c2a447
Zulfiqar Ali <8zali9@gmail.com>
2023-09-19 17:31:52 +0500

    Initial Commit

---
+++ server/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    return '<h3 style="text-align: center; border-top: 3px solid #333; border-bottom: 3px solid #333;">Hard-Coded H3</h3>'
+
+if __name__ == "__main__":
+    app.run()
