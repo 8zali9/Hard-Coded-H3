f795bab59503dc0e75acdcdc4319e9a1c22b8605
Zulfiqar Ali <8zali9@gmail.com>
2023-09-20 01:06:16 +0500

    Final Commit

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def Home():
+def index():
     return '<h3 style="text-align: center; border-top: 3px solid #333; border-bottom: 3px solid #333;">Hard-Coded H3</h3>'
 
 if __name__ == "__main__":
